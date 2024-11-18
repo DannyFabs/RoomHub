b7431e02901d18eb66dab0d449db41e4dea6e806
DannyFabs <aderemifawoyed@gmail.com>
2024-11-17 23:20:40 -0600

    Updated changes required for apk to work

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -50,126 +50,126 @@ class _MyAppState extends ConsumerState<MyApp> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     // Configure Amplify when the app initializes
-    // _configureAmplify();
+    _configureAmplify();
   }
 
-  Widget build(BuildContext context) {
-  List<String> roomMates= ["danny@gmail.com","dd@gmail.com", "lola@gmail.com"];
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: OurTheme().buildTheme(),
-    // home: const ExpenseForm(email: "daohl@myumanitoba.ca", roomId: "The bois"),
-    // home: SettleUp(email: "dan@gmail.com", roomId: "The boys"),
-    // home: SharedExpensesPage(userId: "dan@gmail.com"),
-    home: UserRoom(roomID: "The bois", email: "hungludao@gmail.com"),
-    routes: {
-      '/loginPage': (context) => const OurLogin(),
-      //'/homeNewPage': (context) => const OurHomeNewUser(),
-    },
-  );
-}
+//   Widget build(BuildContext context) {
+//   List<String> roomMates= ["danny@gmail.com","dd@gmail.com", "lola@gmail.com"];
+//   return MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     theme: OurTheme().buildTheme(),
+//     // home: const ExpenseForm(email: "daohl@myumanitoba.ca", roomId: "The bois"),
+//     // home: SettleUp(email: "dan@gmail.com", roomId: "The boys"),
+//     // home: SharedExpensesPage(userId: "dan@gmail.com"),
+//     home: UserRoom(roomID: "The bois", email: "hungludao@gmail.com"),
+//     routes: {
+//       '/loginPage': (context) => const OurLogin(),
+//       //'/homeNewPage': (context) => const OurHomeNewUser(),
+//     },
+//   );
+// }
 
   // / Configures the Amplify libraries and adds the necessary plugins.
-  // Future<void> _configureAmplify() async {
-  //   try {
-  //     // Create an instance of the Cognito Auth plugin
-  //     final auth = AmplifyAuthCognito();
-  //     // Add the Auth plugin to Amplify
-  //     await Amplify.addPlugin(auth);
-  //     // Call Amplify.configure to use the initialized categories in your app
-  //     await Amplify.configure(amplifyconfig);
-  //     setState(() {
-  //       _amplifyConfigured = true; // Update state when configuration is successful
-  //     });
-  //   } on Exception catch (e) {
-  //     // Print an error message if Amplify configuration fails
-  //     safePrint('An error occurred configuring Amplify: $e');
-  //   }
-  // }
-  // //
-  // @override
-  // Widget build(BuildContext) {
-  //   return MaterialApp(
-  //     debugShowCheckedModeBanner: false, // Hides the debug banner
-  //     theme: OurTheme().buildTheme(), // Sets the app's theme
-  //     home: _amplifyConfigured
-  //         ? Consumer(builder: (context, ref, child) {
-  //       final currentUser = ref.watch(authUserProvider); // Watches for user authentication state
-  //       return currentUser.when(
-  //         data: (userId) {
-  //           if (userId == null) {
-  //             return const OurLogin(); // Show login page if no user is logged in
-  //           } else {
-  //             return FutureBuilder<Widget>(
-  //               future: redirectHome(), // Call redirectHome with non-null userEmail
-  //               builder: (context, snapshot) {
-  //                 if (snapshot.connectionState == ConnectionState.waiting) {
-  //                   // Loading state
-  //                   return const Center(
-  //                       child: CircularProgressIndicator());
-  //                 } else if (snapshot.hasError) {
-  //                   // Handle error state
-  //                   return const OurHomeNewUser(); // Show an error page
-  //                 } else {
-  //                   // Successfully fetched the room name, return the widget
-  //                   return snapshot.data ??
-  //                       const OurHomeNewUser(); // Fallback if data is null
-  //                 }
-  //               },
-  //             );
-  //           }
-  //         },
-  //         loading: () {
-  //           // Show a loading indicator while the authentication state is being determined
-  //           return const Center(
-  //               child: CircularProgressIndicator());
-  //         },
-  //         error: (e, st) {
-  //           return const OurHomeNewUser(); // Show an error page in case of an authentication error
-  //         },
-  //       );
-  //     })
-  //         : const Center(
-  //         child: CircularProgressIndicator()), // Show a loading indicator while checking configuration
-  //     routes: {
-  //       '/loginPage': (context) =>
-  //       const OurLogin(), // Defines the route for the login page
-  //     },
-  //   );
-  // }
+  Future<void> _configureAmplify() async {
+    try {
+      // Create an instance of the Cognito Auth plugin
+      final auth = AmplifyAuthCognito();
+      // Add the Auth plugin to Amplify
+      await Amplify.addPlugin(auth);
+      // Call Amplify.configure to use the initialized categories in your app
+      await Amplify.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true; // Update state when configuration is successful
+      });
+    } on Exception catch (e) {
+      // Print an error message if Amplify configuration fails
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
   //
-  // // Redirects user to their home or new user page depending on their room status
-  // Future<Widget> redirectHome() async {
-  //   try {
-  //     // getEmail -- is never going to be null at this point in the application
-  //     userEmail = await getEmail(); // Retrieves the user's email
-  //
-  //     // Send a GET request to the backend to get the user's room information
-  //     var response = await http.get(
-  //       Uri.parse("${url}user/$userEmail/get-room"), // Backend URL to get room details
-  //       headers: {"Content-Type": "application/json"},
-  //     );
-  //     // Log the request URL for debugging
-  //     print("${url}user/$userEmail/get-room");
-  //     // Await the response and extract the room name
-  //     String roomName = await getResponse(response, responseType: 'getUserRoom');
-  //     // Display appropriate page based on the room name
-  //     if (roomName == "NA") {
-  //       return const OurHomeNewUser(); // If no room, redirect to new user page
-  //     } else {
-  //       String nonnullableEmail = userEmail ?? "";
-  //       return OurHome(roomID: roomName, email:nonnullableEmail); // If room exists, redirect to home page
-  //     }
-  //   } on UserException {
-  //     // Handle user-specific exceptions if necessary
-  //   }
-  //   return const OurLogin(); // Fallback in case of error
-  // }
-  //
-  // // Retrieves the user's email from the authentication provider
-  // Future<String?> getEmail() async {
-  //   final authAWSRepo = ref.read(authAWSRepositoryProvider); // Access the auth provider
-  //   userEmail = await authAWSRepo.getUserEmail(ref); // Retrieve email from the provider
-  //   return userEmail;
-  // }
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Hides the debug banner
+      theme: OurTheme().buildTheme(), // Sets the app's theme
+      home: _amplifyConfigured
+          ? Consumer(builder: (context, ref, child) {
+        final currentUser = ref.watch(authUserProvider); // Watches for user authentication state
+        return currentUser.when(
+          data: (userId) {
+            if (userId == null) {
+              return const OurLogin(); // Show login page if no user is logged in
+            } else {
+              return FutureBuilder<Widget>(
+                future: redirectHome(), // Call redirectHome with non-null userEmail
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Loading state
+                    return const Center(
+                        child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    // Handle error state
+                    return const OurHomeNewUser(); // Show an error page
+                  } else {
+                    // Successfully fetched the room name, return the widget
+                    return snapshot.data ??
+                        const OurHomeNewUser(); // Fallback if data is null
+                  }
+                },
+              );
+            }
+          },
+          loading: () {
+            // Show a loading indicator while the authentication state is being determined
+            return const Center(
+                child: CircularProgressIndicator());
+          },
+          error: (e, st) {
+            return const OurHomeNewUser(); // Show an error page in case of an authentication error
+          },
+        );
+      })
+          : const Center(
+          child: CircularProgressIndicator()), // Show a loading indicator while checking configuration
+      routes: {
+        '/loginPage': (context) =>
+        const OurLogin(), // Defines the route for the login page
+      },
+    );
+  }
+
+  // Redirects user to their home or new user page depending on their room status
+  Future<Widget> redirectHome() async {
+    try {
+      // getEmail -- is never going to be null at this point in the application
+      userEmail = await getEmail(); // Retrieves the user's email
+
+      // Send a GET request to the backend to get the user's room information
+      var response = await http.get(
+        Uri.parse("${url}user/$userEmail/get-room"), // Backend URL to get room details
+        headers: {"Content-Type": "application/json"},
+      );
+      // Log the request URL for debugging
+      print("${url}user/$userEmail/get-room");
+      // Await the response and extract the room name
+      String roomName = await getResponse(response, responseType: 'getUserRoom');
+      // Display appropriate page based on the room name
+      if (roomName == "NA") {
+        return const OurHomeNewUser(); // If no room, redirect to new user page
+      } else {
+        String nonnullableEmail = userEmail ?? "";
+        return OurHome(roomID: roomName, email:nonnullableEmail); // If room exists, redirect to home page
+      }
+    } on UserException {
+      // Handle user-specific exceptions if necessary
+    }
+    return const OurLogin(); // Fallback in case of error
+  }
+
+  // Retrieves the user's email from the authentication provider
+  Future<String?> getEmail() async {
+    final authAWSRepo = ref.read(authAWSRepositoryProvider); // Access the auth provider
+    userEmail = await authAWSRepo.getUserEmail(ref); // Retrieve email from the provider
+    return userEmail;
+  }
 }
